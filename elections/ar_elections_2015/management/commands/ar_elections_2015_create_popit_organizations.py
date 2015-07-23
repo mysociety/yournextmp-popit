874a43cb7bf4d58230962febfc27c9805db2ce94
Martín Szyszlican (at YQS server) <martinsz@gmail.com>
2015-07-23 00:34:32 -0400

    Unknown change in create popit organizations. Please review.

--- elections/ar_elections_2015/management/commands/ar_elections_2015_create_popit_organizations.py
+++ elections/ar_elections_2015/management/commands/ar_elections_2015_create_popit_organizations.py
@@ -5,27 +5,35 @@ from candidates.election_specific import PARTY_DATA
 from candidates.models.popit import create_or_update
 from candidates.popit import PopItApiMixin
 
+from slumber.exceptions import HttpClientError
+
 class Command(PopItApiMixin, BaseCommand):
     help = "Create the parties for the 2015 elections in Argentina"
 
     def handle(self, **options):
 
-        # First create all the political parties:
+        try:
+            # First create all the political parties:
 
-        for party_data in PARTY_DATA.all_party_data:
-            create_or_update(
-                self.api.organizations,
-                party_data,
-            )
+            for party_data in PARTY_DATA.all_party_data:
+                import json
+                print "Creating a party with data:", json.dumps(party_data, indent=4)
+                create_or_update(
+                    self.api.organizations,
+                    party_data,
+                )
 
-        # Now we create the organizations that all the posts are
-        # associated with:
+            # Now we create the organizations that all the posts are
+            # associated with:
 
-        for election, election_data in settings.ELECTIONS.items():
-            create_or_update(
-                self.api.organizations,
-                {
-                    'id': election_data['organization_id'],
-                    'name': election_data['organization_name'],
-                }
-            )
+            for election, election_data in settings.ELECTIONS.items():
+                create_or_update(
+                    self.api.organizations,
+                    {
+                        'id': election_data['organization_id'],
+                        'name': election_data['organization_name'],
+                    }
+                )
+        except HttpClientError as hce:
+           print "HttpClientError content is:", hce.content
+           raise
