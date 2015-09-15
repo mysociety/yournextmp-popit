224090162ea7b7de714771bd0cb17160a6a8f575
Mark Longair <mhl@pobox.com>
2015-09-15 18:20:15 +0100

    Make sure the national candidates are returned on address lookup too

--- elections/bf_elections_2015/settings.py
+++ elections/bf_elections_2015/settings.py
@@ -54,3 +54,10 @@ ELECTIONS = {
 }
 
 MAPIT_BASE_URL = 'http://international.mapit.mysociety.org/'
+
+AREAS_TO_ALWAYS_RETURN = [
+    {
+        'area_type_code': 'NATIONAL',
+        'area_id': 0,
+    }
+]
